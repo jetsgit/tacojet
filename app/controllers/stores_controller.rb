3514453d5d22e40d6ef9c04264c423eb9c4edc6e
Jerrold Thompson <jet@whidbey.com>
2016-04-10 11:50:29 -0700

    only list taco and salsa that are actually in a store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -14,20 +14,60 @@ class StoresController < ApplicationController
     end
   end
 
+  def tacos_salsas
+    verify_taco
+    verify_salsa
+    binding.pry
+  end
+
   private
 
   def setup
     set_salsas
     set_tacos
-    @store_salsa_ids = StoreSalsa.all.pluck :store_id, :salsa_id
-    @store_taco_ids = StoreTaco.all.pluck :store_id, :taco_id
+    set_ids
     @stores = Store.pluck :id
     @stores_iter = @stores.dup
     @lunch = []
   end
 
+  def set_ids
+    @store_salsa_ids = StoreSalsa.all.pluck :store_id, :salsa_id
+    @store_taco_ids = StoreTaco.all.pluck :store_id, :taco_id
+  end
+
   def set_tacos
     @tacos = params.require(:taco_ids)
+  end
+
+  def verify_taco
+    tacos_in_store = []
+    set_ids
+    tacos = Taco.all.pluck :id
+    tacos.each do |taco|
+      @store_taco_ids.each do |store|
+        if taco == store[1]
+          tacos_in_store.push taco
+          break
+        end
+      end
+    end
+    @tacos = Taco.find tacos_in_store
+  end
+
+  def verify_salsa
+    salsas_in_store = []
+    set_ids
+    salsas = Salsa.all.pluck :id
+    salsas.each do |salsa|
+      @store_salsa_ids.each do |store|
+        if salsa == store[1]
+          salsas_in_store.push salsa
+          break
+        end
+      end
+    end
+    @salsas = Salsa.find salsas_in_store
   end
 
   def set_salsas
