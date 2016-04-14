75ab9f0200db162113b29a688177f0d133050261
Jerrold Thompson <jet@whidbey.com>
2016-04-14 08:52:41 -0700

    remove unnecessary cities_controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,9 @@
 # Using: Fixtures
 # Using: Shoulda-Matchers
 
+require 'simplecov'
+SimpleCov.start 'rails'
+
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
