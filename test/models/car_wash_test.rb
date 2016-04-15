7b8cc415a51243a3d9dafe4a292bf33cdb275566
Jerrold Thompson <jet@whidbey.com>
2016-04-15 11:49:34 -0700

    happy rubocop

--- test/models/car_wash_test.rb
+++ test/models/car_wash_test.rb
@@ -1,9 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 describe CarWash do
   let(:car_wash) { CarWash.new }
 
-  it "must be valid" do
+  it 'must be valid' do
     value(car_wash).must_be :valid?
   end
 end
