5764fcd75a55a07ae7a7f9906e6a9d7d13da23bc
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-03 07:49:57 +0800

    Delete config directory

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root controller: 'stores', action: :tacos_salsas
-  get 'tacos-salsas' => 'stores#tacos_salsas'
-  post 'taco-heaven' => 'stores#taco_heaven'
-
-end
