0fc269db5dd1fe6138a1af3647d065252a7419a9
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-03 07:52:44 +0800

    Delete test directory

--- test/models/car_wash_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-describe CarWash do
-  let(:car_wash) { CarWash.new }
-
-  it 'must be valid' do
-    value(car_wash).must_be :valid?
-  end
-end
--- test/models/taco_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-describe Taco do
-  should have_many :store_tacos
-  should have_many(:stores).through :store_tacos
-  should validate_uniqueness_of :name
-end
